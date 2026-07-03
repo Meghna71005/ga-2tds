8e288bee75aa71efc3569576bfba3193fe58be69
Meghna Chaudhary <meghna.chaudhary@ibotix.ai>
2026-07-03 17:09:12 +0530

    Initial commit4

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
-EMAIL = "23f2001998@ds.study.iitm.ac.in."
+EMAIL = "23f2001998@ds.study.iitm.ac.in"
 
-Q1_ALLOWED_ORIGIN="https://dash-lvfg78.example.com"
+Q1_ALLOWED_ORIGIN = "https://dash-lvfg78.example.com"
 
-ISSUER="https://idp.exam.local"
-AUDIENCE="tds-dr3t9gnj.apps.exam.local"
-PUBLIC_KEY_PEM= PUBLIC_KEY_PEM = """MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
+ISSUER = "https://idp.exam.local"
+AUDIENCE = "tds-dr3t9gnj.apps.exam.local"
+
+PUBLIC_KEY_PEM = """MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
 WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
@@ -12,16 +13,17 @@ ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
 SI6iyrYbKR0NEBSqq4XkadEjsCs4F1RncsS4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
 dQIDAQAB"""
 
-Q3_PORT=8611
-Q3_WORKERS=3
-Q3_DEBUG=False
-Q3_LOG_LEVEL=debug
+Q3_PORT = 8611
+Q3_WORKERS = 3
+Q3_DEBUG = False
+Q3_LOG_LEVEL = "debug"
 
-Q5_API_KEY= "ak_2mku455wy8bznnkim3ns3jpc"
-Q9_TOTAL_ORDERS=60
-Q9_RATE_LIMIT=19
+Q5_API_KEY = "ak_2mku455wy8bznnkim3ns3jpc"
 
-Q10_ALLOWED_ORIGIN="https://app-yodzqm.example.com"
-Q10_RATE_LIMIT=15
+Q9_TOTAL_ORDERS = 60
+Q9_RATE_LIMIT = 19
+
+Q10_ALLOWED_ORIGIN = "https://app-yodzqm.example.com"
+Q10_RATE_LIMIT = 15
 
 EXAM_PORTAL_ORIGIN = "https://exam.sanand.workers.dev"
